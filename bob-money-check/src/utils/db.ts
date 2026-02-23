850ea5c04221625632c242794d96d48091072fbd
MIKE Juan Victor <juanvictorqwerty@gmail.com>
2026-02-23 13:10:25 +0100

    switch to connection string

--- bob-money-check/src/utils/db.ts
+++ bob-money-check/src/utils/db.ts
@@ -4,11 +4,7 @@ import { Pool } from 'pg';
 import * as schema from '../../drizzle/schema';
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432'),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    connectionString:process.env.DATABASE_URL
 });
 
 export const db = drizzle(pool, { schema });
